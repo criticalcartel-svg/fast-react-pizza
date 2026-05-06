d5a663a0d9964b3aa2017d7019028f01197e5402
KARTEL <criticalcartel@gmail.com>
2026-05-06 15:10:47 +0100

    Add routing and cart functionality with new components
    - Implemented routing using react-router-dom
    - Added Cart, CartItem, CartOverview, and EmptyCart components
    - Created CreateOrder and Order components for order processing
    - Introduced Menu and MenuItem components for displaying pizza options
    - Added utility functions for formatting currency and dates
    - Integrated geolocation and address fetching services
    - Updated styles in index.css for better layout and loading animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/oder/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
